0674643c28e632f37ba56d9b602aedfe75f5934a
Joshua Turner <josh.h.t@live.co.uk>
2024-08-19 22:52:16 +0100

    added images scroll icon plus and minus icons and states crust and size on mobile cart moving onto toppings

--- src/app/components/CartMobile.js
+++ src/app/components/CartMobile.js
@@ -19,7 +19,14 @@ lg:hidden flex flex-col`}
       {/* top */}
       <CartTop />
       {/* cart items */}
-      <div>
+      <div 
+      className={`px-4 flex flex-col gap-y-4 py-2 mr-4 mt-8 h-[60vh]  
+        overflow-y-scroll scrollbar-thin scrollbar-thumb-secondary ${
+          cart.length >= 3 
+          ? 'scrollbar-track-black/10'
+          : 'scrollbar-track-transparent'
+        }`}
+      >
         {cart?.map((pizza, index) => {
         return <CartItem pizza={pizza} key={index} />;
       })}
